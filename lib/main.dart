a24034111f9237a9fe3cadaadea34efe18e68160
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-05-30 13:40:50 +0330

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade400,
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          color: Colors.black,
+          child: GNav(
+            tabs: [
+              GButton(icon: Icons.home_outlined, text: 'Home'),
+              GButton(icon: Icons.favorite_outline, text: 'Likes'),
+              GButton(icon: Icons.search_outlined, text: 'Search'),
+              GButton(icon: Icons.person_outline, text: 'Profile'),
+            ],
+            color: Colors.white,
+            activeColor: Colors.white,
+            backgroundColor: Colors.black,
+            tabBackgroundColor: Colors.grey.shade900,
+            gap: 8,
+          ),
+        ),
+      ),
     );
   }
 }
